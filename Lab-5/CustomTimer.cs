5edc38d73836449209d487aad6069dbe8d283779
Cuyeshi Yunkay <cuyeshi@mail.ru>
2024-05-07 17:35:42 +0300

    Добавьте файлы проекта.

---
+++ Lab-5/CustomTimer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Lab_5
+{
+    public class CustomTimer
+    {
+        private DispatcherTimer timer;
+
+        public event EventHandler TimerElapsed;
+
+        public TimeSpan Interval
+        {
+            get { return timer.Interval; }
+            set { timer.Interval = value; }
+        }
+
+        public bool IsEnabled
+        {
+            get { return timer.IsEnabled; }
+            set { timer.IsEnabled = value; }
+        }
+
+        public CustomTimer(TimeSpan interval)
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = interval;
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            TimerElapsed?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ Lab-5/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Lab_5
+{
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer timer;
+        private int timeoutInSeconds;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeTimer();
+        }
+
+        private void InitializeTimer()
+        {
+            timer = new DispatcherTimer();
+            timer.Tick += TimerTick; // Подписываемся на событие Tick таймера
+            timer.IsEnabled = false; // По умолчанию таймер выключен
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            // Действия, которые нужно выполнить при каждом тике таймера
+            MessageBox.Show("Таймер сработал!");
+            timer.Stop(); // Останавливаем таймер после срабатывания
+        }
+
+        private void StartTimerButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!int.TryParse(TimeoutTextBox.Text, out timeoutInSeconds) || timeoutInSeconds <= 0)
+            {
+                MessageBox.Show("Пожалуйста, введите корректное значение таймаута в секундах.");
+                return;
+            }
+
+            timer.Interval = TimeSpan.FromSeconds(timeoutInSeconds); // Устанавливаем интервал таймера
+            timer.IsEnabled = true; // Включаем таймер
+        }
+    }
+}
